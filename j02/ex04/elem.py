fad7a62e986c3ea0ca7af45730564f8784571a4a
Cyrille GAY <cgay@e1r3p5.42.fr>
2016-11-25 00:42:59 +0100

    save

--- j02/ex04/elem.py
+++ j02/ex04/elem.py
@@ -1,6 +1,14 @@
-
-
-class CoffeeMachine():
+class Elem():
+	def __init__(self,name,attribute,content,balise):
+		self.name = name
+		self.attribute = attribute
+		self.content = content
+		self.balise = balise
+		self.html = ""
+	def add_content(self,add)
+		self.content += add
+	def __str__(self)
+		return (self.html)
 
 
 if __name__ == '__main__':
